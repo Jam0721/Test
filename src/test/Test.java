bee567cd5e84a88ffdb97a12642e978fc298cb06
jack.milner <jack.milner@CLC-LAB209-05.TCSD.tooelesd.org>
2018-09-04 14:19:01 -0600

    First commit

---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+/**
+ * hello world program print hello world in console
+ */
+package test;
+
+/**
+ * @author jack.milner
+ * 9/18
+ * hello world
+ * 
+ */
+public class Test {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("hello World ");
+
+	}
+
+}
